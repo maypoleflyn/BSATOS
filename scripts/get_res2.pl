e3fba66b0a30b9128f23b9755852021c162a503d
Fei Shen <352363546@qq.com>
2018-08-19 23:06:01 +0200

    Delete get_res2.pl

--- scripts/get_res2.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl
-
-
- system("sort -k 1\,1 -k 2\,2n $ARGV[0] > tmp1;sort -k 1\,1 -k 2\,2n  $ARGV[1] > tmp2;filter -k cn tmp1 tmp2 > res;cut -f 1-4,7-8 res > res_end;rm tmp1 tmp2 res"); 
